9202c33b287a1b011c9afc2c84c3cc5cebf2d706
Volodymyr Tartakovskyi <vvt2001@ukr.net>
2021-12-02 16:50:18 +0200

    TopicList start

--- src/components/TopicList.jsx
+++ src/components/TopicList.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+axios.defaults.baseURL = 'http://forum.loc/api/'
 
 
 function TopicList() {
+
+  const [threads, setThreads] = useState([])
+
+  useEffect(() => {
+    axios
+    .get('/thread')
+    .then(response => {
+      setThreads(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
 
   return (
     <>
